c681d9b90f94f1c33a946570e13faf982faa7689
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2025-11-26 17:36:55 +0000

    Add Gazelle I/O library

---
+++ io/ETABS/Types.fs
@@ -0,0 +1,10 @@
+namespace Gazelle.IO.ETABS
+
+type StoreyRenderProps =
+  { Name: string
+    Height: float
+    IsMaster: bool
+    MasterRefName: string
+    IsSplicedAbove: bool
+    SpliceHeight: float
+    Color: int }
