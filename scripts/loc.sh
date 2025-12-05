96a6ab15b5b8f692cc9fc2a1739248fd84b6c5b2
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2025-12-05 12:04:14 +0000

    Refactor code

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -3,20 +3,22 @@
 # suggest that a codebase has become too complex given the scoped software 
 # requirements.
 
-lines_of_code=$(cloc ./src ./cli ./tests --include-ext=fs --csv --quiet | grep SUM | awk -F, '{print $5}')
-
-echo "$lines_of_code lines of F# code across cli, src, and tests directories."
-echo ""
-echo "  src: $(cloc ./src --include-ext=fs --csv --quiet | grep SUM | awk -F, '{print $5}')."
-echo "  tests: $(cloc ./tests --include-ext=fs --csv --quiet | grep SUM | awk -F, '{print $5}')."
-echo "  cli: $(cloc ./cli --include-ext=fs --csv --quiet | grep SUM | awk -F, '{print $5}')."
-echo ""
-
+loc_limit=2500
 RED='\033[0;31m'
 DEFAULT='\033[0m'
-loc_limit=2500
 
-if [ $lines_of_code -gt $loc_limit ]; then
+loc=$(cloc ./src ./cli ./tests --include-ext=fs --csv --quiet | awk -F, '/SUM/ {print $5}')
+
+echo "$loc lines of F# code across cli, src, and tests directories."
+echo ""
+
+for dir in src tests cli; do
+  count=$(cloc ./$dir --include-ext=fs --csv --quiet | awk -F, '/SUM/ {print $5}')
+  echo "  $dir: $count."
+done
+echo ""
+
+if [ "$loc" -gt "$loc_limit" ]; then
   echo -e "${RED}Line count exceeded imposed limit: $loc_limit lines.${DEFAULT}"
   exit 1
 fi
